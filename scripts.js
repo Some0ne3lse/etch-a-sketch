78dbbdadc9de49a3850ff12a15df131bac7f4d78
Some0ne3lse <143652507+Some0ne3lse@users.noreply.github.com>
2024-03-17 20:32:31 +0000

    Make button do something

--- scripts.js
+++ scripts.js
@@ -5,13 +5,30 @@ document.documentElement.style.cssText = `
     box-sizing: border-box;
 `;
 
+let gridDimensions = 16;
+
 const body = document.querySelector('body');
 body.style.cssText = 'background-color: black; display: flex; flex-direction: column; height: 100%; margin: 0; padding: 0; box-sizing: border-box; align-items: center;';
 
+
 const newButton = document.createElement('button');
 newButton.textContent = 'Change grid!';
-newButton.style.cssText = 'width: 100px; margin-top: 100px; margin-bottom: 100px;'
-body.appendChild(newButton)
+newButton.classList.add('changeGrid')
+newButton.style.cssText = 'width: 100px; margin-top: 100px; margin-bottom: 100px;';
+body.appendChild(newButton);
+
+// const theButton = document.querySelector('.changeGrid');
+// theButton.addEventListener(onclick, changeDimensions);
+
+function changeDimensions() {
+  alert('Test')
+  // let myDialog = document.createElement('dialog');
+  // let text = document.createTextNode('Please enter the dimensions you want (MAX 100)');
+  // myDialog.appendChild(text);
+  // myDialog.showModal;
+}
+
+newButton.onclick = changeDimensions;
 
 
 const container = document.createElement('div');
@@ -19,13 +36,13 @@ container.classList.add('container');
 container.style.cssText = 'display: flex; flex-direction: column; justify-content: center; align-items: center; width: 660px; height: 660px;'
 body.appendChild(container);
 
-for (let i = 0; i < 16; i++) {
+for (let i = 0; i < gridDimensions; i++) {
 
   const sixteenCol = document.createElement('div');
   sixteenCol.classList.add('sixteenCol');
   sixteenCol.style.cssText = 'display: flex; flex-direction: row; width: fit-content; background-color: white; height:100%; width: 100%;'
   container.appendChild(sixteenCol);
-  for (let u = 0; u < 16; u++) {
+  for (let u = 0; u < gridDimensions; u++) {
     const sixteenRow = document.createElement('div');
     sixteenRow.classList.add('sixteenRow');
     sixteenRow.style.cssText = 'display: flex; flex-direction: column; flex: auto; height: 100%; width: 100%'
