d84b5e03e35f0a6a2f3748949605abf80546be07
Some0ne3lse <143652507+Some0ne3lse@users.noreply.github.com>
2024-03-18 22:44:15 +0000

    Made grid able to change

--- scripts.js
+++ scripts.js
@@ -8,6 +8,8 @@ document.documentElement.style.cssText = `
 
 let gridDimensions = 16;
 
+
+
 const body = document.querySelector('body');
 body.style.cssText = 'background-color: black; display: flex; flex-direction: column; height: 100%; margin: 0; padding: 0; box-sizing: border-box; align-items: center;';
 
@@ -23,6 +25,8 @@ function changeDimensions(gridDimensions) {
   if (newDimension != null && newDimension <= 100 && newDimension > 0) {
     gridDimensions = newDimension;
     createGrid(gridDimensions);
+  } else {
+    alert('Not a valid option!')
   }
 }
 
@@ -33,16 +37,30 @@ container.classList.add('container');
 container.style.cssText = 'display: flex; flex-direction: column; justify-content: center; align-items: center; width: 660px; height: 660px;'
 body.appendChild(container);
 
-body.onload = createGrid;
 
+// for (let i = 0; i < 16; i++) {
+//   const initialCol = document.createElement('div');
+//   initialCol.classList.add('sixteenCol');
+//   initialCol.style.cssText = 'display: flex; flex-direction: row; width: fit-content; background-color: white; height:100%; width: 100%;'
+//   container.appendChild(initialCol);
+//   for (let u = 0; u < 16; u++) {
+//     const initialRow = document.createElement('div');
+//     initialRow.classList.add('sixteenRow');
+//     initialRow.style.cssText = 'display: flex; flex-direction: column; flex: auto; height: 100%; width: 100%'
+//     initialCol.appendChild(initialRow);
+//   }
+// }
 function createGrid(gridDimensions) {
   for (let i = 0; i < gridDimensions; i++) {
-
+    const removeCol = document.querySelector('.sixteenCol');
+    removeCol.remove();
     const sixteenCol = document.createElement('div');
     sixteenCol.classList.add('sixteenCol');
     sixteenCol.style.cssText = 'display: flex; flex-direction: row; width: fit-content; background-color: white; height:100%; width: 100%;'
     container.appendChild(sixteenCol);
     for (let u = 0; u < gridDimensions; u++) {
+      const removeRow = document.querySelector('.sixteenRow');
+      removeRow.remove();
       const sixteenRow = document.createElement('div');
       sixteenRow.classList.add('sixteenRow');
       sixteenRow.style.cssText = 'display: flex; flex-direction: column; flex: auto; height: 100%; width: 100%'
@@ -58,4 +76,6 @@ function createGrid(gridDimensions) {
     });
   }
 }
+
+
 
